edd84e3b77b3650a974031046287605d05238e88
Mike Boers <github@mikeboers.com>
2018-10-24 14:22:24 -0400

    Add Python3 to PyPI tags.

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
         'Natural Language :: English',
         'Operating System :: OS Independent',
         'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
     
