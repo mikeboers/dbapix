0cd454205ce5e57fbebe406ab9eea500aee812d8
Mike Boers <github@mikeboers.com>
2019-01-23 10:01:54 -0500

    Release v1.0.0.
    This is the first release. Woo!

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
 
     name='dbapix',
-    version='0.1.0',
+    version='1.0.0',
     description="A unification of, and extension to, several DB-API 2.0 drivers.",
 
     url='http://github.com/mikeboers/dbapix',
